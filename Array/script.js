e302c33fd7b27ed8a9a5bb7c88f3f491063d9b4f
njscd <your-email@example.com>
2025-03-14 16:24:18 +0500

    array 2

--- Array/script.js
+++ Array/script.js
@@ -1,48 +1,48 @@
-// let alphabet = ["a", "b", "c", "d", "c", "c", "c", "c"];
-// //Array Property
-// //length
-// console.log("length:", alphabet.length);
+let alphabet = ["a", "b", "c", "d", "c", "c", "c", "c"];
+//Array Property
+//length
+console.log("length:", alphabet.length);
 
-// //Array Methods
-// //concat():
-// let newAlphabet = alphabet.concat(["k", "l", "m"]);
-// console.log("concat:", newAlphabet);
+//Array Methods
+//concat():
+let newAlphabet = alphabet.concat(["k", "l", "m"]);
+console.log("concat:", newAlphabet);
 
-// //push():
-// alphabet.push("e");
-// console.log("push:", alphabet);
+//push():
+alphabet.push("e");
+console.log("push:", alphabet);
 
-// //pop():
-// let lastElement = alphabet.pop();
-// console.log("pop:", alphabet);
-// console.log("removed element:", lastElement);
+//pop():
+let lastElement = alphabet.pop();
+console.log("pop:", alphabet);
+console.log("removed element:", lastElement);
 
-// //shift():
-// let firstElement = alphabet.shift();
-// console.log("shift:", alphabet);
-// console.log("Removed element:", firstElement);
+//shift():
+let firstElement = alphabet.shift();
+console.log("shift:", alphabet);
+console.log("Removed element:", firstElement);
 
-// //indexOf():
-// let indexOfElement = alphabet.indexOf("c");
-// console.log("index of 'c':", indexOfElement);
+//indexOf():
+let indexOfElement = alphabet.indexOf("c");
+console.log("index of 'c':", indexOfElement);
 
-// //unshift():
-// alphabet.unshift("z");
-// console.log("unshift:", alphabet);
+//unshift():
+alphabet.unshift("z");
+console.log("unshift:", alphabet);
 
-// //slice():
-// let sliceArray = alphabet.slice(2, 6);
-// console.log("slice:", sliceArray);
-// console.log(alphabet);
+//slice():
+let sliceArray = alphabet.slice(2, 6);
+console.log("slice:", sliceArray);
+console.log(alphabet);
 
-// //splice():
-// alphabet.splice(1, 2, "x", "y");
-// console.log("splice:", alphabet);
+//splice():
+alphabet.splice(1, 2, "x", "y");
+console.log("splice:", alphabet);
 
-// //find
-// let findExample = alphabet.find((item) => item === "c");
-// console.log(findExample);
+//find
+let findExample = alphabet.find((item) => item === "c");
+console.log(findExample);
 
-// //filter
-// let filterExample = alphabet.filter((item) => item === "c");
-// console.log(filterExample);
+//filter
+let filterExample = alphabet.filter((item) => item === "c");
+console.log(filterExample);
